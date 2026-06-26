a03cb9d771b0cf2aa0509c29a1ef12657b0088a5
PiconEffect <julien.brunello@gmail.com>
2026-06-26 09:30:41 +0200

    Implement SolisClient class with get_data method

---
+++ services/core/app/solis_client.py
@@ -0,0 +1,9 @@
+class SolisClient:
+    def get_data(self):
+        return {
+            "pv_power": 3200,
+            "battery_soc": 80,
+            "grid_power": -400,
+            "load_power": 2600
+        }
+``

0bf97544a71ae7c6d0f44cfb068ae181c21eba08
PiconEffect <julien.brunello@gmail.com>
2026-06-26 10:24:55 +0200

    Update mqtt.py

--- services/core/app/mqtt.py
+++ services/core/app/mqtt.py
@@ -2,10 +2,14 @@ import json
 import os
 import paho.mqtt.client as mqtt
 
+
 class MqttClient:
     def __init__(self):
         self.client = mqtt.Client()
-        self.client.connect(os.getenv("MQTT_HOST"), int(os.getenv("MQTT_PORT")))
+        self.client.connect(
+            os.getenv("MQTT_HOST"),
+            int(os.getenv("MQTT_PORT")),
+        )
         self.client.loop_start()
 
     def publish(self, topic, payload):
